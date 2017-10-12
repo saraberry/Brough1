5a08d9e037c04eb945ef498541d1470812673463
Marco <marcoaldana25@yahoo.com>
2017-10-11 19:08:25 -0500

    Created User Class

---
+++ GroupFinal/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupFinal
+{
+    public class User
+    {
+        private int userID, userZIP;
+        private string userFirstName, userLastName, userAddress, userCity, userState, userPhone;
+
+        public User()
+        {
+
+        }
+
+        public User(int userID, string userFirstName, string userLastName, string userAddress, string userCity, string userState, int userZip, string userPhone)
+        {
+            this.userID = UserID;
+            this.userFirstName = UserFirstName;
+            this.userLastName = UserLastName;
+            this.userAddress = UserAddress;
+            this.userCity = UserCity;
+            this.userState = UserState;
+            this.userZIP = UserZip;
+            this.userPhone = UserPhone;
+        }
+
+        public int UserID { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserAddress { get; set; }
+        public string UserCity { get; set; }
+        public string UserState { get; set; }
+        public string UserPhone { get; set; }
+        public int UserZip { get; set; }
+    }
+}
