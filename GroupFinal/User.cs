c787f43040aa9e58cd16d0ab970a6c74ec58988c
Marco <marcoaldana25@yahoo.com>
2017-10-11 19:32:00 -0500

    Updated User Class

--- GroupFinal/User.cs
+++ GroupFinal/User.cs
@@ -8,32 +8,36 @@ namespace GroupFinal
     public class User
     {
         private int userID, userZIP;
-        private string userFirstName, userLastName, userAddress, userCity, userState, userPhone;
+        private string userFirst, userLast, userAddress, userCity, userState, userPhone, userRole, userCC;
 
         public User()
         {
 
         }
 
-        public User(int userID, string userFirstName, string userLastName, string userAddress, string userCity, string userState, int userZip, string userPhone)
+        public User(int userID, string userFirst, string userLast, string userAddress, string userCity, string userState, int userZip, string userPhone, string userRole, string userCC)
         {
             this.userID = UserID;
-            this.userFirstName = UserFirstName;
-            this.userLastName = UserLastName;
+            this.userFirst = UserFirst;
+            this.userLast = UserLast;
             this.userAddress = UserAddress;
             this.userCity = UserCity;
             this.userState = UserState;
             this.userZIP = UserZip;
             this.userPhone = UserPhone;
+            this.userRole = UserRole;
+            this.userCC = UserCC;
         }
 
         public int UserID { get; set; }
-        public string UserFirstName { get; set; }
-        public string UserLastName { get; set; }
+        public string UserFirst { get; set; }
+        public string UserLast { get; set; }
         public string UserAddress { get; set; }
         public string UserCity { get; set; }
         public string UserState { get; set; }
         public string UserPhone { get; set; }
         public int UserZip { get; set; }
+        public string UserRole { get; set; }
+        public string UserCC { get; set; }
     }
 }
