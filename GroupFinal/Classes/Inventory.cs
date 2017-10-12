2972b214f45f20573e46e745d0fa2d409715537e
Marco <marcoaldana25@yahoo.com>
2017-10-11 19:43:33 -0500

    Created Inventory Class, moved User class to Classes Folder

---
+++ GroupFinal/Classes/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupFinal.Classes
+{
+    public class Inventory
+    {
+        private string itemType;
+        private int itemAmount;
+        private int itemMax;
+        private int itemMin;
+        private double itemPrice;
+        private int itemID;
+
+        public Inventory()
+        {
+        }
+
+        public Inventory(string itemType, int itemAmount, int itemMax, int itemMin, double itemPrice, int itemID)
+        {
+            this.itemType = ItemType;
+            this.itemAmount = ItemAmount;
+            this.itemMax = ItemMax;
+            this.itemMin = ItemMin;
+            this.itemPrice = ItemPrice;
+            this.itemID = ItemID;
+        }
+
+        public string ItemType { get; set; }
+        public int ItemAmount { get; set; }
+        public int ItemMax { get; set; }
+        public int ItemMin { get; set; }
+        public double ItemPrice { get; set; }
+        public int ItemID { get; set; }
+    }
+}
