dbb90d8160b27c619f0bf2475b55d268a0888494
Brandon <bjrough1@gmail.com>
2017-10-11 19:47:45 -0500

    pizza class finished

--- GroupFinal/Classes/Pizza.cs
+++ GroupFinal/Classes/Pizza.cs
@@ -5,8 +5,51 @@ using System.Web;
 
 namespace GroupFinal
 {
-    public class Pizza : Product
+    public class Pizza : Products
     {
-        
+        //declaring variables
+        private string pizzaToppings, pizzaCheese, pizzaSauce, pizzaCrust, pizzaExtras, pizzaSize;
+        private double pizzaPrice;
+
+        //empty constructor
+        public Pizza()
+        {
+            pizzaToppings = "";
+            pizzaCheese = "";
+            pizzaSauce = "";
+            pizzaCrust = "";
+            pizzaExtras = "";
+            pizzaSize = "";
+            pizzaPrice = 0;
+        }
+
+        //constructor with all the variables
+        public Pizza(string pizzaToppings, string pizzaCheese, string pizzaSauce, string pizzaCrust, string pizzaExtras, string pizzaSize, double pizzaPrice)
+        {
+            this.pizzaToppings = pizzaToppings;
+            this.pizzaCheese = pizzaCheese;
+            this.pizzaSauce = pizzaSauce;
+            this.pizzaCrust = pizzaCrust;
+            this.pizzaExtras = pizzaExtras;
+            this.pizzaSize = pizzaSize;
+            this.pizzaPrice = pizzaPrice;
+        }
+
+        //get and sets for all the variables, these may need to be changed/just the basic ones for right now
+
+
+        public string pizzaToppings { get; set; }
+
+        public string pizzaCheese { get; set; }
+
+        public string pizzaSauce { get; set; }
+
+        public string pizzaCrust { get; set; }
+
+        public string pizzaExtras { get; set; }
+
+        public string pizzaSize { get; set; }
+
+        public double pizzaPrice { get; set; }
     }
 }
